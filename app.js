0a3e4e4aff173019a403d2e2d993f881b7732b0a
rwakaramapatient <thesecond1018@gmail.com>
2026-02-14 20:57:05 +0200

    after review, that's the result

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-let name = "cherife";
-function add(a,b) {
-    return a+b;
+let userName = "cherife";
+
+function add(a, b) {
+  return a + b;
 }
-console.log(add(1,4));
+console.log("add(1,4) =", add(1, 4));
+
 function stage(age) {
-    if(age <= 18){
-        return "minor";
-    }
-    else{
-        return "adult";
-    }
+  if (age <= 18) return "minor";
+  return "adult";
 }
+console.log("stage(18) =", stage(18));
+console.log("stage(19) =", stage(19));
+
+console.log('"5" + 2 =', "5" + 2);
+console.log('"5" - 2 =', "5" - 2);
