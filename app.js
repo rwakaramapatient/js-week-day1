3c2c9cc34a4edf100c06362b9ca14274b5598424
rwakaramapatient <thesecond1018@gmail.com>
2026-02-14 16:04:34 +0200

    Js-Week-1

---
+++ app.js
@@ -0,0 +1,13 @@
+let name = "cherife";
+function add(a,b) {
+    return a+b;
+}
+console.log(add(1,4));
+function stage(age) {
+    if(age <= 18){
+        return "minor";
+    }
+    else{
+        return "adult";
+    }
+}
